6f36353614407e89c3710e905a43d2c0923a4e3d
iand11 <ian.driscoll.o@gmail.com>
2017-05-28 19:48:36 -0700

    Finish blog

--- config/routes.rb
+++ config/routes.rb
@@ -1,26 +1,24 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :posts do 
-    member do 
-      get 'likes' => 'post#likes'
-    end 
+    resources :comments
   end 
   root 'posts#index'
-# GET    /posts/:id/likes posts#likes
-# GET    /posts          posts#index
-# POST   /posts          posts#create
-# GET    /posts/new      posts#new
-# GET    /posts/:id/edit posts#edit
-# GET    /posts/:id      posts#show
-# PATCH  /posts/:id      posts#update
-# PUT    /posts/:id      posts#update
-# DELETE /posts/:id      posts#destroy
-
-  resources :likes
- #POST   /likes           likes#create
- #GET    /likes/new       likes#new
- #GET    /likes/:id/edit  likes#edit
- #GET    /likes/:id       likes#show
- #PATCH  /likes/:id       likes#update
- #PUT    /likes/:id       likes#update
- #DELETE /likes/:id       likes#destroy
+#     post_comments GET    /posts/:post_id/comments(.:format)          comments#index
+#                   POST   /posts/:post_id/comments(.:format)          comments#create
+#  new_post_comment GET    /posts/:post_id/comments/new(.:format)      comments#new
+# edit_post_comment GET    /posts/:post_id/comments/:id/edit(.:format) comments#edit
+#      post_comment GET    /posts/:post_id/comments/:id(.:format)      comments#show
+#                   PATCH  /posts/:post_id/comments/:id(.:format)      comments#update
+#                   PUT    /posts/:post_id/comments/:id(.:format)      comments#update
+#                   DELETE /posts/:post_id/comments/:id(.:format)      comments#destroy
+#             posts GET    /posts(.:format)                            posts#index
+#                   POST   /posts(.:format)                            posts#create
+#          new_post GET    /posts/new(.:format)                        posts#new
+#         edit_post GET    /posts/:id/edit(.:format)                   posts#edit
+#              post GET    /posts/:id(.:format)                        posts#show
+#                   PATCH  /posts/:id(.:format)                        posts#update
+#                   PUT    /posts/:id(.:format)                        posts#update
+#                   DELETE /posts/:id(.:format)                        posts#destroy
+#              root GET    /                                           posts#index
 end 
